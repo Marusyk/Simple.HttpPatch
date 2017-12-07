715bd29aff07f36c19d72a6c79b9d7eb4879af18
Marusyk <romamarusyk@gmail.com>
2017-12-08 01:06:07 +0200

    Add support Patch for collections. #2

---
+++ src/Simple.HttpPatch/PatchCollection.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace Simple.HttpPatch
+{
+    public class PatchCollection<TModel> : List<Patch<TModel>> where TModel : class, new()
+    {
+        public void Apply(IList<TModel> deltas)
+        {
+            if (deltas == null)
+            {
+                throw new ArgumentNullException(nameof(deltas));
+            }
+
+            for (int i = 0; i < deltas.Count; i++)
+            {
+                this[i].Apply(deltas[i]);
+            }
+        }
+    }
+}
